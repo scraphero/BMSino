54168f80adf438bd7b74a126e98a934445aa23f4
Pablo Hernandez <pablo.hache.eme@gmail.com>
2019-02-24 22:53:48 +0100

    print WATIOS adaptado para string

--- battery_monitor/battery_monitor.ino
+++ battery_monitor/battery_monitor.ino
@@ -3,15 +3,15 @@
 int tiempo_real;
 
 /*PINES*/
-const int PIN_AMP_METER = A0;          //PIN AMPERIMETRO
-const int PIN_VOLT_METER = A7;         //PIN VOLTIMETRO
+const int PIN_AMP_METER = A0;            //PIN AMPERIMETRO
+const int PIN_VOLT_METER = A7;           //PIN VOLTIMETRO
 
-/*SENSOR HALL*/
-const float CORRECT_VOLT_HALL = -2.50; //Para restar el voltage que registra el sensor hall cuando la corriente es = 0
-const float SENSIBILIDAD = 65;         //sensibilidad en Voltios/Amperio para sensor de 5A
+/*AMPERIMETRO SENSOR HALL*/
+const float CORRECT_VOLT_HALL = -2.50;   //Para restar el voltage que registra el sensor hall cuando la corriente es = 0
+const float SENSIBILIDAD = 65;           //sensibilidad en Voltios/Amperio para sensor de 5A
 
 /*VOLTIMETRO*/
-const float CORRECT_VOLT_BATT = 49.8;  //La bateria proporciona alrededor de 50V a la que fisicamente se le ha aplicado un divisor de tension de 10:1 asique para 50V de bateria le llegan 5
+const float CORRECT_VOLT_BATT = 49.8;    //La bateria proporciona alrededor de 50V a la que fisicamente se le ha aplicado un divisor de tension de 10:1 asique para 50V de bateria le llegan 5
 
 /*BATERIA*/
 const int TIEMPO_DE_LECTURA = 100;       //sera el tiempo en milisegundos 
@@ -21,7 +21,7 @@ const int DELAY_MEDICION = 1;            //En este caso el valor es 1 porque que
 int porcentaje_bateria = 0;              //Almacenara el porcentaje de bateria que se calculara a partir de bateriaRestante posteriormente
 int long autonomia_segundos_totales = 0; //Medira la autonomia instantanea expresada en segundos
 int short autonomia_segundos = 0;        //Autonomia en segundos restantes 
-int long autonomia_minutos_totales = 0;
+int long autonomia_minutos_totales = 0;  
 int short autonomia_minutos = 0;
 int long autonomia_horas_totales = 0;
 
@@ -34,15 +34,15 @@ void medicion_periodica()
   bateriaRestante = bateriaRestante - corriente_hall;                           //Cada vez que se ejecute esta linea se restara el valor medido de corriente a las unidades totales de bateria
   porcentaje_bateria = (bateriaRestante / 162000)*100;                          //Sencilla operacion para calcular en forma de porcentaje la bateria restante a partir de esas unidades sin magnitud que nos hemos inventado
   
-  autonomia_segundos_totales = abs( bateriaRestante / corriente_hall ) ;                //autonomia_segundos_totales sera tiempo restante de bateria en segundos, sera un valor instantaneo
+  autonomia_segundos_totales = abs( bateriaRestante / corriente_hall ) ;        //autonomia_segundos_totales sera tiempo restante de bateria en segundos, sera un valor instantaneo
   autonomia_segundos = autonomia_segundos_totales % 60;                         //autonomia en segundos como dato complementario de horas y minutos es el resultado de calcular el resto de la division de los segundos totales entre 60
   autonomia_minutos_totales = autonomia_segundos_totales / 60;                  //minutos restantes totales es el valor resultante de dividir los segundos totales entre 60
   autonomia_minutos = autonomia_minutos_totales % 60;                           //autonomia en minutos como dato complementario de horas y segundos es el resultado de calcular el resto de la division de los minutos totales entre 60
-  autonomia_horas_totales = autonomia_minutos_totales / 60;                     //
+  autonomia_horas_totales = autonomia_minutos_totales / 60;                     //horas de autonomia es el resultado de dividir los minutos totales entre 60
 
 
-//                                 PRINTS PARA DEBUG
-//{ No descomentar esta linea
+
+//{ Prints DEBUG
 
   //Serial.print("tiempo real:");
   //Serial.print(tiempo_real);
@@ -58,37 +58,54 @@ void medicion_periodica()
   
   //Serial.print("    Voltaje sensor: ");
   //Serial.print(voltajeSensor);                     //Voltaje del sensor Hall
-//} No descomentar esta linea
+//}
 
+//{ Serial Print PORCENTAJE BATERIA
   if (porcentaje_bateria < 100) { Serial.print(0); }  //para mostrar 1 cero a la izquierda en caso de que el valor sea de 2 cifras esto es necesario para mantener el string estable
   if (porcentaje_bateria < 10) { Serial.print(00); }  //para mostrar 2 ceros a la izquierda en caso de que el valor sea de 1 cifras
   Serial.print(porcentaje_bateria);                   //Serial print porcentaje bateria
   Serial.print("% ");
+//}
 
+//{ Serial print AUTONOMIA
   //autonomia_horas_totales = 0 ; //DEBUG
   if ( autonomia_horas_totales < 100 ) { Serial.print(0); }   //otra vez ceros a la izquierda
   if ( autonomia_horas_totales < 10 ) { Serial.print(00); }   //
   Serial.print( autonomia_horas_totales ) ;  //Horas restantes de autonomia   
-  Serial.print("h ");
+  Serial.print("h");
   
   if ( autonomia_minutos < 10 ) { Serial.print(0); }   //para mostrar 1 ceros a la izquierda en caso de que el valor sea de 1 cifras
   Serial.print ( abs ( autonomia_minutos ) );                   //Minutos restantes de autonomia
-  Serial.print("m ");
+  Serial.print("m");
   
   if (autonomia_segundos < 10) { Serial.print(0); }  //para mostrar 1 ceros a la izquierda en caso de que el valor sea de 1 cifras
   Serial.print(autonomia_segundos);                  //Segundos restantes de autonomia
   Serial.print("s");
+//}
 
+//{ Serial Print POTENCIA
+  //Watt = 11111.1;                                                    //DEBUG
+  Serial.print(" W:");                                             //imprime el indicador de watios para hacer mas comprensible el string
+  if ( Watt >= 0 ) { Serial.print( "+" ); }                        //imprime signo positivo
+  if ( Watt <  0 ) { Serial.print( "-" ); }                        //o negativo segun corresponda
+  Watt = abs(Watt);                                                //ahora Watt se convierte en un valor absoluto pues no es deseable que se imprima mas vezes el signo
+  if ( Watt >= 9999.99 ) { Watt = 9999.99 ; }                      //impide que se supere este valor pues que aumente el numero de cifras estropearia el string
+  if ( (Watt < 1000 ) && ( Watt >= 100 ) ) { Serial.print("0"); }  //para mostrar 1 ceros a la izquierda en caso de que el valor sea de 1 cifras
+  if ( (Watt < 100) && (Watt >= 10) ) { Serial.print("00"); }      //para mostrar 2 ceros a la izquierda en caso de que el valor sea de 1 cifras
+  if ( Watt < 10 ) { Serial.print("000"); }                        //para mostrar 3 ceros a la izquierda en caso de que el valor sea de 1 cifras
+  Serial.print( Watt );                                            //Print Potencia
+//}
 
-  Serial.print("    W: ");
-  Serial.print(Watt);                                //Print Potencia
-  
-  Serial.print("    V: ");
+//{ Serial Print TENSION BATERIA
+  Serial.print(" V:");
   Serial.print(voltajeBatt);                         //Print Tension
-  
-  if ( corriente_hall > 0 ) { serial.print( "+" ) ; }
+//}
+
+//{ Serial Print CORRIENTE BATERIA
+  if ( corriente_hall > 0 ) { Serial.print( "+" ) ; }
   Serial.print(" A:");
   Serial.println ( corriente_hall, 2 ) ;    //Print Corriente
+//}
 
   tiempo_medicion = tiempo_medicion + DELAY_MEDICION ;    //Como hemos comentado antes esta linea le añade el tiempo que queremos que retarde entre lecturas
 }
